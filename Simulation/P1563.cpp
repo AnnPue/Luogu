dea2d205f303f568e401b3f32d70b891154d67b4
AnnPue <hurui200612@outlook.com>
2026-05-10 11:42:01 +0800

    添加玩具谜题的实现代码

---
+++ Simulation/P1563.cpp
@@ -0,0 +1,40 @@
+// [NOIP 2016 提高组] 玩具谜题
+#include <iostream>
+#include <vector>
+#include <utility> // for std::pair
+
+int main()
+{
+    int n, m; // n: 小人数量；m: 指令数
+    std::cin >> n >> m;
+    // first: 小人朝向。0：朝圈内；1：朝圈外
+    // second: 小人名字
+    std::vector<std::pair<char, std::string>> men;
+    for (int i = 0; i < n; ++i)
+    {
+        char ori;
+        std::string name;
+        std::cin >> ori >> name;
+        men.emplace_back(ori, name);
+    }
+    char move;          // 数数的方向。0：向左数；1：向右数
+    int step, tman = 0; // step: 数的步数；tman: 当前小人编号（0~n-1）
+    while (m--)
+    {
+        std::cin >> move >> step;
+        // 小人朝外且向左数 或 小人朝内且向右数 代表按逆时针方向数
+        // 小人朝内且向左数 或 小人朝外且向右数（同或关系）代表按顺时针方向数
+        if (move == men[tman].first)
+        { // 若为顺时针，则步数为负
+            step = -step;
+        }
+        int x = tman + step;
+        if (x < 0)
+        {
+            tman = x + n; // 负数时加上n得到正确的小人编号
+            continue;
+        }
+        tman = x % n; // 大于等于0时直接取余得到正确的小人编号
+    }
+    std::cout << men[tman].second;
+}
