2172c6c5d1e8555b44ed9c3b0df82b087ef16641
AnnPue <hurui200612@outlook.com>
2026-05-31 22:14:46 +0800

    整数序列 最大连续子序列和

---
+++ Algo/DP/P1115.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, b;
+    cin >> n >> b;
+    int ans = -1e5;
+    for (int i = 2; i <= n; ++i)
+    {
+        int a;
+        cin >> a;
+        b = max(a, b + a);
+        ans = max(ans, b);
+    }
+    cout << ans;
+    return 0;
+}
+
+/*
+// b[i] 表示截止到 i 时，第 i 个数所在的有效序列的元素和。
+int main(){
+   cin>>n;
+   for(i=1;i<=n;i++){
+       cin>>a[i];
+       if(i==1) b[i]=a[i];
+       else b[i]=max(a[i],b[i-1]+a[i]);
+       ans=max(ans,b[i]);
+   }
+   cout<<ans;
+   return 0;
+}
+*/
