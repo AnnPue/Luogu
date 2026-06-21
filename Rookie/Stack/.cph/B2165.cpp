f73267fb2848d368ab4df4e01fb15ffc97e38e8e
AnnPue <hurui200612@outlook.com>
2026-06-21 18:35:59 +0800

    isValid 函数化

--- Rookie/Stack/.cph/B2165.cpp
+++ Rookie/Stack/.cph/B2165.cpp
@@ -2,43 +2,37 @@
 #include <unordered_map>
 #include <iostream>
 using namespace std;
+unordered_map<char, char> m = {
+    {'(', ')'}, {'[', ']'}, {'{', '}'}};
+bool isValid(const string &s)
+{
+    string st;
+    for (char c : s)
+    {
+        if (c == '(' || c == '[' || c == '{')
+            st.push_back(c);
+        else
+        {
+            if (st.empty())
+                return false;
+            char top = st.back();
+            if (c != m[top])
+                return false;
+            st.pop_back(); // 匹配成功，出栈
+        }
+    }
+    return st.empty();
+}
 
 int main()
 {
     int t;
     cin >> t;
-    unordered_map<char, char> m = {
-        {'(', ')'}, {'[', ']'}, {'{', '}'}};
     while (t--)
     {
         string s;
         cin >> s;
-        bool isValid = true;
-        stack<char> st;
-        for (const char &c : s)
-        {
-            if (c == '(' || c == '[' || c == '{')
-                st.push(c);
-            else
-            {
-                if (st.empty())
-                {
-                    isValid = false;
-                    break;
-                }
-                char now = st.top();
-                if (c != m[now])
-                {
-                    isValid = false;
-                    break;
-                }
-                st.pop();
-            }
-        }
-        if (!st.empty())
-            isValid = false;
-
-        if (isValid)
+        if (isValid(s))
             cout << "YES\n";
         else
             cout << "NO\n";
